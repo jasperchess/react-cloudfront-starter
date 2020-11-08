5b9747e5bf12d42eee5caff87015c6ee976492fa
Jasper Chesselet <chesselet.jasper@gmail.com>
2020-11-08 09:22:17 +0200

    Install using a workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,6 @@ read_var() {
 
 yarn --frozen-lockfile
 yarn build --if-present
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-aws --version
 sudo yarn global add serverless
 serverless deploy -s dev
 BUCKET=$(read_var WebSiteBucket .env)
