ca86fd347f98991467889b788cc3add507f5419b
Jasper Chesselet <chesselet.jasper@gmail.com>
2020-11-08 11:14:43 +0200

    Cache invalidation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,11 @@ yarn --frozen-lockfile
 yarn build --if-present
 sudo yarn global add serverless
 serverless deploy -s dev
+
 BUCKET=$(read_var WebSiteBucket .env)
 echo "---------------- Publishing to bucket ${BUCKET} ----------------"
 aws s3 sync build/ "s3://${BUCKET}" --acl public-read
+
+DISTRIBUTION=$(read_var DistributionID .env)
+echo "---------------- Invalidating ${DISTRIBUTION} cache ----------------"
+aws cloudfront create-invalidation --distribution-id "${DISTRIBUTION}" --paths *
